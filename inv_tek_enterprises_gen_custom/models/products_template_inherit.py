0316738fd6e68868044b79a3dfba65d808b6ac83
kaleem789 <kaleemabbas789@gmail.com>
2026-04-23 14:50:59 +0500

    product archive

--- inv_tek_enterprises_gen_custom/models/products_template_inherit.py
+++ inv_tek_enterprises_gen_custom/models/products_template_inherit.py
@@ -6,6 +6,20 @@ class ProductTemplateInherit(models.Model):
 
     brand_ids = fields.Many2many('brand.name', string='Brand Name')
 
+    active = fields.Boolean('Active', default=True)
+
+    def action_approve(self):
+        self.active = True
+
+    def action_confirm(self):
+        self.active = True
+
+    @api.model
+    def create(self, vals):
+        record = super(ProductTemplateInherit, self).create(vals)
+        record.active = False
+        return record
+
 
 class BrandConfig(models.Model):
     _name = 'brand.name'
